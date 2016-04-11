e43a7fc3e50bc4443cd1bddf02d28e55788d70ec
Richard Kim <rkim43@uic.edu>
2016-04-10 22:34:34 -0500

    Added Keywords
    commit

--- MiniWatson/src/Keywords.java
+++ MiniWatson/src/Keywords.java
@@ -12,6 +12,15 @@ public class Keywords {
 			 put("movie", 3);
 			 put("film", 3);
 			 put("directed", 3);
+			 put("credits", 3);
+			 put("animation", 3);
+			 put("anime", 3);
+			 put("cartoon", 3);
+			 put("documentary", 3);
+			 put("remake", 2);
+			 put("silent film", 3);
+			 put("satire", 1);
+			 put("video", 3);
 		 }
 	};
 
@@ -24,13 +33,22 @@ public class Keywords {
 			put("did", 1);
 			put("directed", 3);
 			put("by", 2);
+			put("played", 2);
+			put("featured", 2);
 		}
 	};
 	
 	@SuppressWarnings("serial")
 	public static Map<String,Integer> MovieAdjectives = new HashMap<String,Integer>() {
 		 {
-			 
+			 put("low-budget", 3);
+			 put("charismatic", 2);
+			 put("disappointing", 2);
+			 put("romantic", 2);
+			 put("big-budget", 3);
+			 put("box-office", 3);
+			 put("sequel", 3);
+			 put("horror", 2);
 		 }
 	};
 	
@@ -38,6 +56,7 @@ public class Keywords {
 	public static Map<String,Integer> MusicNouns = new HashMap<String,Integer>() {
 		 {
 			 put("album", 3);
+			 put("instrument", 3);
 			 put("track", 3);
 			 put("song", 3);
 			 put("artist", 3);
@@ -45,6 +64,7 @@ public class Keywords {
 			 put("pop", 3);
 			 put("dance", 3);
 			 put("genre", 2);
+			 put("band", 3);
 		 }
 	};
 
@@ -59,12 +79,20 @@ public class Keywords {
 			put("record", 2);
 			put("preform", 2);
 			put("by", 2);
+			put("played", 2);
 		}
 	};
 	
 	@SuppressWarnings("serial")
 	public static Map<String,Integer> MusicAdjectives = new HashMap<String,Integer>() {
 		 {
+			 put("acoustic", 3);
+			 put("bass", 3);
+			 put("instrumental", 3);
+			 put("sharp", 2);
+			 put("flat", 2);
+			 put("funky", 3);
+			 put("jazzy", 3);
 			 
 		 }
 	};
@@ -82,6 +110,9 @@ public class Keywords {
 			 put("ocean", 2);
 			 put("deeper", 2);
 			 put("population", 3);
+			 put("hurricane", 3);
+			 put("tornado", 3);
+			 put("earthquake", 3);
 		 }
 	};
 
@@ -93,6 +124,8 @@ public class Keywords {
 			put("appear", 1);
 			put("was", 1);
 			put("in", 1);
+			put("travel", 3);
+			put("traveled", 3);
 		}
 	};
 	
@@ -103,6 +136,10 @@ public class Keywords {
 			 put("highest", 3);
 			 put("deepest", 3);
 			 put("higher", 3);
+			 put("rocky", 3);
+			 put("dangerous", 2);
+			 put("hot", 2);
+			 put("cold", 2);
 		 }
 	};
 }
